dd228c71b726d3410c8a4bd7b8c0bff1212523a9
Chris Hager <chris@linuxuser.at>
2025-07-02 13:06:02 +0200

    prefix metrics with mempool_dumpster_

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -7,24 +7,24 @@ import (
 )
 
 var (
-	txReceived      = metrics.NewCounter("tx_received_total")
-	txReceivedFirst = metrics.NewCounter("tx_received_first")
-	txReceivedTrash = metrics.NewCounter("tx_received_trash")
+	txReceived      = metrics.NewCounter("mempool_dumpster_tx_received_total")
+	txReceivedFirst = metrics.NewCounter("mempool_dumpster_tx_received_first")
+	txReceivedTrash = metrics.NewCounter("mempool_dumpster_tx_received_trash")
 
-	clickhouseErrors           = metrics.NewCounter("clickhouse_errors_total")
-	clickhouseErrorsBatchSave  = metrics.NewCounter("clickhouse_errors_batch_save_total")
-	clickhouseBatchSaveRetries = metrics.NewCounter("clickhouse_batch_save_retries_total")
-	clickhouseBatchSaveGiveup  = metrics.NewCounter("clickhouse_batch_save_giveup_total")
-	clickhouseBatchSaveSuccess = metrics.NewCounter("clickhouse_batch_save_success_total")
+	clickhouseErrors           = metrics.NewCounter("mempool_dumpster_clickhouse_errors_total")
+	clickhouseErrorsBatchSave  = metrics.NewCounter("mempool_dumpster_clickhouse_errors_batch_save_total")
+	clickhouseBatchSaveRetries = metrics.NewCounter("mempool_dumpster_clickhouse_batch_save_retries_total")
+	clickhouseBatchSaveGiveup  = metrics.NewCounter("mempool_dumpster_clickhouse_batch_save_giveup_total")
+	clickhouseBatchSaveSuccess = metrics.NewCounter("mempool_dumpster_clickhouse_batch_save_success_total")
 )
 
 const (
-	TxReceivedSourceLabel      = `tx_received_total{source="%s"}`
-	TxReceivedFirstSourceLabel = `tx_received_first{source="%s"}`
-	TxReceivedTrashLabel       = `tx_received_trash{source="%s"}`
+	TxReceivedSourceLabel      = `mempool_dumpster_tx_received_total{source="%s"}`
+	TxReceivedFirstSourceLabel = `mempool_dumpster_tx_received_first{source="%s"}`
+	TxReceivedTrashLabel       = `mempool_dumpster_tx_received_trash{source="%s"}`
 
-	ClickhouseBatchSaveTimeLabel = `clickhouse_batch_save_duration_milliseconds{type="%s"}`
-	ClickhouseEntriesSavedLabel  = `clickhouse_entries_saved_total{type="%s"}`
+	ClickhouseBatchSaveTimeLabel = `mempool_dumpster_clickhouse_batch_save_duration_milliseconds{type="%s"}`
+	ClickhouseEntriesSavedLabel  = `mempool_dumpster_clickhouse_entries_saved_total{type="%s"}`
 )
 
 func IncTxReceived(source string) {
